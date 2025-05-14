f0a8fae86254ea4871219b9e4a2afc169782a8f2
saiganeshpalla <ganesh@stockholmitacademy.org>
2025-05-14 12:38:43 +0530

    added the code

---
+++ reactPractice/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './nav'
+import Content from './content'
+function App(){
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default App
---
+++ reactPractice/src/content.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+function Content(){
+    return(
+        <div>
+            <h1>iam the main content</h1>
+
+        </div>
+    )
+}
+
+export default Content
