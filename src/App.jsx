24fb12f655cee250b06e8194ee8c2f0fe3b88ab3
saiganeshpalla <ganesh@stockholmitacademy.org>
2026-01-21 11:45:56 +0530

    hello world

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Header from './Header'
+
+function App(){
+    return(
+        <div>
+            <NavBar/>
+            <Header/>
+
+        </div>
+    )
+}
+
+export default App 
---
+++ src/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function NavBar(){
+    return(
+        <div>
+            <h1>iam the NavBar </h1>
+
+            
+
+        </div>
+    )
+}
+
+export default NavBar
